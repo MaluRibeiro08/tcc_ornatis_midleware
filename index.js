4847d961669e9a7d4156fdd149e731c949eeff10
Maria Luiza Ribeiro Teixeira <maluribeiro08@gmail.com>
2022-05-11 11:28:43 -0300

    Listagem de perfil de conta adm e create de funcionario

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { send } = require('express/lib/response');
 const axios = require('axios').default;
 
 const app = express();
@@ -6,8 +7,77 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-// const contaController = require('./Controller/ContaAdministradoraController');
-// app.use('/', contaController);
+//funcoes
+const getDados_funcionamento = (obj_dados_funcionamento) =>
+{
+    // console.log(response.data.data.dados_funcionamento[6]);
+    let arr_dados_funcionamento = [];
+
+    let contador = 1;
+    while (contador <= 7)
+    {
+        if(obj_dados_funcionamento[contador]=== undefined)
+        {
+            arr_dados_funcionamento.push('')
+        }
+        else
+        {
+            // console.log("dia"+ contador + " é definido")
+            arr_dados_funcionamento.push(obj_dados_funcionamento[contador])
+        }
+        contador = contador + 1;
+    }
+    // console.log(arr_dados_funcionamento);
+    return arr_dados_funcionamento;
+}
+
+const getTaxas_cancelamento = (obj_taxas_cancelamento) =>
+{
+    // console.log(response.data.data.dados_funcionamento[6]);
+    let arr_taxas_cancelamentos = [];
+
+    let contador = 1;
+    while (contador <= tamanh_array_taxas)
+    {
+        if(obj_taxas_cancelamento[contador]=== undefined)
+        {
+            arr_taxas_cancelamentos.push('')
+        }
+        else
+        {
+            // console.log("dia"+ contador + " é definido")
+            arr_taxas_cancelamentos.push(obj_taxas_cancelamento[contador])
+        }
+        contador = contador + 1;
+    }
+    // console.log(arr_taxas_cancelamentos);
+    return arr_taxas_cancelamentos;
+}
+
+const getDados_pagamento = (obj_dados_pagamento) =>
+{
+    // console.log(response.data.data.dados_funcionamento[6]);
+    let arr_dados_pagamento = [];
+
+    let contador = 1;
+    while (contador <= 6)
+    {
+        if(obj_dados_pagamento[contador]=== undefined)
+        {
+            // console.log("forma"+ contador + " é indefinido")
+            // arr_dados_pagamento.push('')
+        }
+        else
+        {
+            // console.log("forma"+ contador + " é definido")
+            arr_dados_pagamento.push(contador.toFixed())
+        }
+        contador = contador + 1;
+    }
+    // console.log(arr_dados_pagamento);
+    return arr_dados_pagamento;
+}
+
 
 // ADM - CONTA_ADM
     //CADASTRO
@@ -21,7 +91,8 @@ app.use(express.urlencoded({extended:true}));
             (
                 function (response) 
                 {
-                    res.send('INSERÇÃO');
+                    res.statusCode = 200;
+                    res.send(response.data);
                     console.log(response.data);
                 }
             )
@@ -30,9 +101,9 @@ app.use(express.urlencoded({extended:true}));
     //LISTAGEM DE PERFIL
         app.get('/contaAdministradora/listarPerfil/:id_empresa', (req, res)=>{
             
-            console.log(req.params);
+            // console.log(req.params);
             let id_empresa = req.params.id_empresa;
-            console.log(id_empresa);
+            // console.log(id_empresa);
 
             const urlListagemDadosPerfil = `http://localhost/tcc_ornatis_back-end/api-ornatis/rotas/adm/contaAdministradora/?id_empresa=${id_empresa}&acao=carregarPerfil`;
 
@@ -41,8 +112,50 @@ app.use(express.urlencoded({extended:true}));
             (
                 function (response) 
                 {
-                    res.send(response.data.data);
+                    console.log("teste");
                     console.log(response.data.data);
+                    const dados_conta_adm_api = response.data.data;
+
+                    const taxa_unica = dados_conta_adm_api.taxa_cancelamento_empresa.taxa_unica_cancelamento;
+                    let taxas_personalizadas = null;
+                    if(taxa_unica === undefined)
+                    {
+                        taxas_personalizadas = Object.values(dados_conta_adm_api.taxa_cancelamento_empresa);
+                    }                 
+                    const contaAdministradora = 
+                    {
+                            
+                            nome_fantasia: 'teste 1005',
+                            cnpj: '23456789765432',
+                            telefone: '12345678654321',
+                            biografia: 'Biografia 1005',
+                            intervalo_tempo_padrao_entre_servicos: 15,
+                            taxa_unica_cancelamento: 12,
+                            imagem_perfil: null,
+                            nome_usuario_instagram: null,
+                            link_facebook: null,
+                            cep: '12345678',
+                            bairro: 'SPairro 1005',
+                            rua: 'Rua 1005',
+                            numero: '12',
+                            complemento: 'Complemento 1',
+                            id_cidade: 3510609,
+                            nome_cidade: 'Carapicuíba',
+                            nome_estado: 'São Paulo',
+                            sigla_estado: 'SP',
+
+                          dados_funcionamento: getDados_funcionamento(dados_conta_adm_api.dados_funcionamento),
+                          dados_formas_pagamento: getDados_pagamento(dados_conta_adm_api.dados_pagamento.formas_aceitas),
+                          observacoes_pagamento: dados_conta_adm_api.dados_pagamento.observacoes_pagamento,
+                          taxa_unica_cancelamento: taxa_unica,
+                          dados_taxa_cancelamento: taxas_personalizadas,
+                        
+                        }
+
+
+                   
+                    res.send(contaAdministradora);
+                    // console.log(response.data.data);
                 }
             )
         });
@@ -59,7 +172,8 @@ app.use(express.urlencoded({extended:true}));
             (
                 function (response) 
                 {
-                    res.send('INSERÇÃO');
+                    res.statusCode = 200;
+                    res.send(response.data);
                     console.log(response.data);
                 }
             )
